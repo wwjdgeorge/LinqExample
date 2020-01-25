06d1802e8c893e50e7b9db74077874e29c2d2b86
wwjdgeorge <wwjdgeorgemathew@gmail.com>
2020-01-25 15:08:57 -0600

    test

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExample
+{
+    class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? CustomerTypeId { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
